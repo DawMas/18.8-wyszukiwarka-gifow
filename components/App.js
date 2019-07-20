72a6e8dad06447188826288d06b2f3af9cecd46b
Dawid Maslankiewicz <maslankiewiczd@gmail.com>
2019-07-20 19:14:58 +0200

    Add promise to Gif download section in App

--- components/App.js
+++ components/App.js
@@ -15,33 +15,49 @@ App = React.createClass({
         this.setState({
             loading: true  // 2.
         });
-        this.getGif(searchingText, function (gif) {  // 3.
-            this.setState({  // 4
-                loading: false,  // a
-                gif: gif,  // b
-                searchingText: searchingText  // c
+        this.getGif(searchingText)   // 3.
+            .then(gif => {
+                this.setState({  // 4
+                    loading: false,  // a
+                    gif: gif,  // b
+                    searchingText: searchingText  // c
+                });
+            })
+            .catch(error => {
+                console.log("Nie można załadować gifa")
             });
-        }.bind(this));
+
     },
 
-    getGif: function (searchingText, callback) {  // 1.
-       var GIPHY_API_URL = 'https://api.giphy.com' ;
-       var GIPHY_PUB_KEY = 'CP9CSwsmOuZ0IMUul9Rzs1uyy4mud1dz';
-        var url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;  // 2.
-        var xhr = new XMLHttpRequest();  // 3.
-        xhr.open('GET', url);
-        xhr.onload = function () {
-            if (xhr.status === 200) {
+
+    getGif: function (searchingText) {  // 1.
+        return new Promise(function (resolve, reject) {
+
+            var GIPHY_API_URL = 'https://api.giphy.com';
+            var GIPHY_PUB_KEY = 'CP9CSwsmOuZ0IMUul9Rzs1uyy4mud1dz';
+            var url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;  // 2.
+            var xhr = new XMLHttpRequest();  // 3.
+
+            xhr.onload = function () {
+
                 var data = JSON.parse(xhr.responseText).data; // 4.
                 var gif = {  // 5.
                     url: data.fixed_width_downsampled_url,
                     sourceUrl: data.url
                 };
-                callback(gif);  // 6.
+                if (xhr.status === 200) {
+                    resolve(gif);
+                }
+                else {
+                    reject(error);
+                }
             }
-        };
-        xhr.send();
+            xhr.open('GET', url);
+            xhr.send();
+        });
+
     },
+
 
     render: function () {
 
